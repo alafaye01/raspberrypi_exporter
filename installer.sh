c71fdc6f1c9002d30cbbefd6f5448427645f4fc2
Alexandre Lafaye <alafaye@logitech.com>
2023-06-05 10:25:53 +0200

    Update pathes

--- installer.sh
+++ installer.sh
@@ -6,10 +6,11 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-curl -sL "https://github.com/fahlke/raspberrypi_exporter/archive/master.zip" > "/tmp/raspberrypi_exporter.zip"
+# TODO Move to the eventual logitech repo
+curl -sL "https://github.com/alafaye01/raspberrypi_exporter/archive/master.zip" > "/tmp/raspberrypi_exporter.zip"
 unzip -qq -o "/tmp/raspberrypi_exporter.zip" -d "/tmp"
 
-mkdir -p "/var/lib/node_exporter/textfile_collector"
+# mkdir -p "/var/lib/node_exporter/textfile_collector"
 
 mv "/tmp/raspberrypi_exporter-master/raspberrypi_exporter" "/usr/local/sbin/"
 chmod +x "/usr/local/sbin/raspberrypi_exporter"
